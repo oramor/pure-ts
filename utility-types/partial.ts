0bdbc57c15b6da33d0da4bfd1af63332d2d9b1e4
Roman Rodin <romaro83@gmail.com>
2022-05-31 13:42:40 +0300

    partial

---
+++ utility-types/partial.ts
@@ -0,0 +1,16 @@
+interface Product {
+    name: string;
+    description: string;
+    properties?: {
+        [key: string]: string | number;
+    }[];
+}
+
+// All properties becomes optional
+const a: Partial<Product> = {}
+
+type CustomPartial<T> = {
+    [K in keyof T]?: T[K];
+}
+
+const b: CustomPartial<Product> = {};
