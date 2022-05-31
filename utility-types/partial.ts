b99320aa714e45524a7d6067bda7e8347988b47f
Roman Rodin <romaro83@gmail.com>
2022-05-31 14:54:39 +0300

    add partial

--- utility-types/partial.ts
+++ utility-types/partial.ts
@@ -1,16 +1,18 @@
-interface Product {
-    name: string;
-    description: string;
-    properties?: {
-        [key: string]: string | number;
-    }[];
+export namespace partial {
+    interface Product {
+        name: string;
+        description: string;
+        properties?: {
+            [key: string]: string | number;
+        }[];
+    }
+
+    // All properties becomes optional
+    const a: Partial<Product> = {}
+
+    type CustomPartial<T> = {
+        [K in keyof T]?: T[K];
+    }
+
+    const b: CustomPartial<Product> = {};
 }
-
-// All properties becomes optional
-const a: Partial<Product> = {}
-
-type CustomPartial<T> = {
-    [K in keyof T]?: T[K];
-}
-
-const b: CustomPartial<Product> = {};
